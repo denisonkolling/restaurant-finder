7dd767db8b163a564a77fbf43f51112f12601362
Denison Kolling <denisonkolling@gmail.com>
2024-04-27 10:52:40 -0300

    style: update carousell cards margin

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -1,26 +1,27 @@
-import { styled } from 'styled-components';
+import { styled } from "styled-components";
 
 const Card = styled.div`
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
   padding: 5px;
-	width: 90px;
-	height: 90px;
-	border-radius: 6px;
-	background-image: url(${(props) => props.photo});
-	background-size: cover;
+  width: 90px;
+  height: 90px;
+  border-radius: 6px;
+  background-image: url(${(props) => props.photo});
+  background-size: cover;
+  margin: 5px;
 `;
 
 const Title = styled.span`
-font-family: Roboto;
-color: #ffffff;
-font-size: 16px;
+  font-family: Roboto;
+  color: #ffffff;
+  font-size: 14px;
 `;
 
 const ImageCard = ({ photo, title }) => (
-	<Card photo={photo}>
-		<Title>{title}</Title>
-	</Card>
+  <Card photo={photo}>
+    <Title>{title}</Title>
+  </Card>
 );
 
 export default ImageCard;
